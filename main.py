5ecff9d825bf54bdc950a444b7b2e4cf98db6daa
Leohawk <Leoyang158@gmail.com>
2021-11-20 21:35:33 -0600

    update on the function button

--- main.py
+++ main.py
@@ -9,7 +9,39 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         loadUi("mainPage.ui", self)
         self.show()
+        self.uiOrderSubmit.clicked.connect(self.orderResult)
+        self.uiCustomerSubmit.clicked.connect(self.customerResult)
+        self.uiProductSubmit.clicked.connect(self.productResult)
+        self.uiFraud.clicked.connect(self.advancedQuery)
+        self.advancePage = AdvancedPage()
+        self.resultTable = ResultPage()
 
+    def orderResult(self):
+        self.resultTable.show()
+
+    def customerResult(self):
+        self.resultTable.show()
+
+    def productResult(self):
+        self.resultTable.show()
+
+    def advancedQuery(self):
+        self.advancedPage.show()
+
+class AdvancedPage(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(AdvancedPage, self).__init__()
+        loadUi("advancedQuery.ui", self)
+        # self.resultTable = ResultPage()
+        # self.uiSubmitAdvanced.clicked.connect(self.advancedResult)
+
+    def advancedResult(self):
+        self.resultTable.show()
+
+class ResultPage(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(ResultPage, self).__init__()
+        loadUi("resultTable.ui", self)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
