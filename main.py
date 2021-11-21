664f23ef1e0b3d5d4d819df70f033d0d9272dcad
Leohawk <Leoyang158@gmail.com>
2021-11-21 16:10:25 -0600

    Improve Some New Page

--- main.py
+++ main.py
@@ -4,51 +4,64 @@ from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainPage(QtWidgets.QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super(MainPage, self).__init__()
         loadUi("mainPage.ui", self)
-        self.show()
         self.uiOrderSubmit.clicked.connect(self.orderResult)
         self.uiCustomerSubmit.clicked.connect(self.customerResult)
         self.uiProductSubmit.clicked.connect(self.productResult)
         self.uiFraud.clicked.connect(self.advancedQuery)
-        self.advancePage = AdvancedPage()
-        self.resultTable = ResultPage()
 
     def orderResult(self):
-        self.resultTable.show()
+        orderView = ResultPage()
+        widget.addWidget(orderView)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
     def customerResult(self):
-        self.resultTable.show()
+        customerView = ResultPage()
+        widget.addWidget(customerView)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
     def productResult(self):
-        self.resultTable.show()
+        productView = ResultPage()
+        widget.addWidget(productView)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
     def advancedQuery(self):
-        self.advancedPage.show()
+        advanceView = ResultPage()
+        widget.addWidget(advanceView)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
 class AdvancedPage(QtWidgets.QMainWindow):
     def __init__(self):
         super(AdvancedPage, self).__init__()
         loadUi("advancedQuery.ui", self)
         # self.resultTable = ResultPage()
-        # self.uiSubmitAdvanced.clicked.connect(self.advancedResult)
+        self.uiSubmitAdvanced.clicked.connect(self.advancedResult)
 
     def advancedResult(self):
-        self.resultTable.show()
+        resview = ResultPage()
+        widget.addWidget(resview)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
 class ResultPage(QtWidgets.QMainWindow):
     def __init__(self):
         super(ResultPage, self).__init__()
         loadUi("resultTable.ui", self)
+        self.uiSearchButton.clicked.connect(self.newSearch)
+
+    def newSearch(self):
+        newView = MainPage()
+        widget.addWidget(newView)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MainWindow()
-    # widget = QtWidgets.QStackedWidget()
-    # widget.addWidget(window)
-    # widget.setFixedHeight(620)
-    # widget.setFixedWidth(480)
-    # window.show()
-    app.exec()
+    window = MainPage()
+    widget = QtWidgets.QStackedWidget()
+    widget.addWidget(window)
+    widget.setFixedWidth(700)
+    widget.setFixedHeight(720)
+    widget.show()
+    app.exec_()
